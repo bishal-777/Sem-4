64395fff69bd3537410387436d1a91a54020c15f
bishal-777 <bishal777giri@gmail.com>
2025-05-23 22:34:30 +0545

    added lab-1 for data communication

---
+++ data_comms/lab-1/Continuous_Cosine.m
@@ -0,0 +1,9 @@
+a=5;
+b=4;
+t=0:0.001:1;
+A=cos(2*pi*f*t);
+plot(t,A,'RED');
+xlabel('time');
+ylabel('amplitude');
+title('Continuous Cosine/Bishal Giri/24');
+%grid on;
---
+++ data_comms/lab-1/Continuous_Sine.m
@@ -0,0 +1,9 @@
+a=5;
+b=4;
+t=0:0.001:1;
+A=sin(2*pi*f*t);
+plot(t,A);
+xlabel('time');
+ylabel('amplitude');
+title('Continuous Sine/Bishal Giri/24');
+%grid on;
